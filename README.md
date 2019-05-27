59134a8567f1fe5d6c5ba20a4c880cdcd7402f64
Sean Morrison <sean1972@gmail.com>
2019-05-27 10:12:57 -0700

    Examples of Classes and Subclasses

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 - [Redux DevTools](https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en)
 - Node.js
 - Git
+
+## Some VS Shortcuts that I use frequently
+
+- [Visual Studio Code Shortcuts](https://code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf)
+- Duplicate line: `SHIFT + ALT + UP/DOWN ARROW`
+- Move line up or down: `OPT + UP/DOWN ARROW`
+- Delete line: `SHIFT + CMD + K`
--- js/app.js
+++ js/app.js
@@ -85,6 +85,39 @@ console.log('The ' + species + ' says ' + sound + ' and needs ' + liquid + '!');
 
 // CLASSES
 
+class Cat {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    return `Hello, my name is ${this.name} and I am ${this.age}.`;
+  }
+}
+
+const cat1 = new Cat('Macy', 4);
+const cat2 = new Cat('Milo', 3);
+
+console.log(`${cat1.greet()} ${cat2.greet()}`);
+
 // SUBCLASSES
+
+class Customer extends Cat {
+  // Inherit the properties of the Cat class by using super().
+  // Needs to be inside of a constructor.
+  constructor(name, age, balance) {
+    super(name, age);
+    this.balance = balance;
+  }
+
+  info() {
+    return `${this.name} owes $${this.balance}.00`;
+  }
+}
+
+const customer1 = new Customer('Kevin', 32, 300);
+
+console.log(customer1.info());
 
 // MODULES
